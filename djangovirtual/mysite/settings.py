f5d4a324f3bbffb3dde2a2f949e71d715d5ad997
PANGKL <99307270+PANGKL@users.noreply.github.com>
2026-03-17 16:23:11 +0800

    edit

--- djangovirtual/mysite/settings.py
+++ djangovirtual/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)55^(gmxftcys=^fh-(=2zg5=4#5m9rtoaoiy2a2p8&r%reha0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pangkamlok.pythonanywhere.com', '.pythonanywhere.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
